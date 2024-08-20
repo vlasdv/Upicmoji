ee052d4fa336665c8a97fb43a9f76ad0bf724adb
Dmitrii Vlasov <vlasdev@icloud.com>
2024-08-20 15:56:28 +0200

    Add project file

---
+++ Upicmoji/UpicmojiApp.swift
@@ -0,0 +1,17 @@
+//
+//  UpicmojiApp.swift
+//  Upicmoji
+//
+//  Created by Dmitrii Vlasov on 20/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct UpicmojiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
