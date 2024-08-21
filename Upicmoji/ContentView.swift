3c2ea5e1c67b686befed1d00b1ac52cc6b7baeb3
Dmitrii Vlasov <vlasdev@icloud.com>
2024-08-21 16:22:49 +0200

    Ger rid of MemojiTextView-related code

--- Upicmoji/ContentView.swift
+++ Upicmoji/ContentView.swift
@@ -7,54 +7,6 @@
 
 import SwiftUI
 
-class MemojiTextView: UITextView {
-    override func didMoveToWindow() {
-        super.didMoveToWindow()
-        if window != nil {
-            self.becomeFirstResponder()
-        }
-    }
-}
-
-struct MemojiTextViewRepresentable: UIViewRepresentable {
-    @Binding var image: UIImage?
-
-    func makeUIView(context: Context) -> MemojiTextView {
-        let textView = MemojiTextView()
-        textView.allowsEditingTextAttributes = true
-        textView.delegate = context.coordinator
-        return textView
-    }
-
-    func updateUIView(_ uiView: MemojiTextView, context: Context) {}
-
-    func makeCoordinator() -> Coordinator {
-        Coordinator(self)
-    }
-
-    class Coordinator: NSObject, UITextViewDelegate {
-        var parent: MemojiTextViewRepresentable
-
-        init(_ parent: MemojiTextViewRepresentable) {
-            self.parent = parent
-        }
-
-        func textViewDidChange(_ textView: UITextView) {
-            if let attributedText = textView.attributedText {
-                attributedText.enumerateAttribute(.attachment, in: NSRange(location: 0, length: attributedText.length), options: []) { value, range, _ in
-                    if let attachment = value as? NSTextAttachment {
-                        if let image = attachment.image {
-                            parent.image = image
-                            textView.resignFirstResponder()
-                        }
-                    }
-                }
-            }
-            textView.text = ""
-        }
-    }
-}
-
 struct ContentView: View {
     @State private var selectedColor = Color.blue
     @State private var selectedImage: UIImage?
